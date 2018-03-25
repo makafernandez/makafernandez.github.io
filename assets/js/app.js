64f0712f68ba815b458775fcecaffb441a7f01a6
missmakita <mfernanlav@gmail.com>
2018-03-25 08:24:11 -0300

    structure

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  $('div.section').click(function() {
-    $(this).switchClass('m s', 'l');
-    $('div.section').not(this).switchClass('m l', 's');
-  });
-
-  $('.carousel').carousel();
-});
